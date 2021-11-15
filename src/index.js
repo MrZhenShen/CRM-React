94f71e20c2554b0ba2c3702354c13a3175500196
mrzhenshen <mrzhenshen@gmail.com>
2021-11-15 23:10:38 +0200

    install bootstrap and bootstrap-icons

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap-icons/font/bootstrap-icons.css'
+import 'bootstrap/dist/js/bootstrap.min'
+
 ReactDOM.render(
   <App />,
   document.getElementById('root')
