6be86a1714fe3fdb0e64f5c2912e96c6ffec7d7e
mrzhenshen <mrzhenshen@gmail.com>
2021-11-18 22:12:39 +0200

    generate ClientInfoModal component

---
+++ src/components/ClientInfoModal/ClientInfoModal.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './ClientInfoModal.scss';
+
+const ClientInfoModal = () => (
+  <div className="ClientInfoModal">
+    ClientInfoModal Component
+  </div>
+);
+
+ClientInfoModal.propTypes = {};
+
+ClientInfoModal.defaultProps = {};
+
+export default ClientInfoModal;
