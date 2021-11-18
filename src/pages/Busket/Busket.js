c3aa1110107b810a47f0b93da302c21527428d51
mrzhenshen <mrzhenshen@gmail.com>
2021-11-18 21:27:35 +0200

    generate Busket

---
+++ src/pages/Busket/Busket.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './Busket.scss';
+
+const Busket = () => (
+  <div className="Busket">
+    Busket Component
+  </div>
+);
+
+Busket.propTypes = {};
+
+Busket.defaultProps = {};
+
+export default Busket;
