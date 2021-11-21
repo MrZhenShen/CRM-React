66acf9c5c83b6a17739908aeefbe84b3dfb8f9d7
mrzhenshen <mrzhenshen@gmail.com>
2021-11-21 21:09:23 +0200

    setup Routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import './App.scss';
 
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import Landing from './pages/Landing/Landing'
+import AdminPanel from './pages/AdminPanel/AdminPanel'
+import Busket from './pages/Busket/Busket'
+
 function App() {
   return (
     <div className="app">
-      This is my App
+      <Router>
+        <Header />
+        <div className="app-container">
+          <Routes>
+            <Route exact path="/" element={<Landing />} />
+            <Route path="/landing" element={<Landing />} />
+            <Route path="/busket" element={<Busket />} />
+            <Route path="/admin-panel" element={<AdminPanel />} />
+          </Routes>
+        </div>
+        {/* <Footer /> */}
+      </Router>
     </div>
   );
 }
