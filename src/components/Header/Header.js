4e15e899eb3ed674d98c0135ac937570406a35ac
mrzhenshen <mrzhenshen@gmail.com>
2021-11-21 21:09:34 +0200

    setup header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './Header.scss';
+import { NavLink } from 'react-router-dom'
 
 const Header = () => (
-  <div className="Header">
-    Header Component
-  </div>
+  <nav className="navbar navbar-expand-lg navbar-light bg-light">
+    <div className="container-fluid">
+      <a className="navbar-brand">Yellow Minders</a>
+      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+        <div className="navbar-nav">
+          <NavLink to='/landing' className="navbar-link ms-4" activeClassName="active">Home</NavLink>
+          <NavLink to='/busket' className="navbar-link ms-4" activeClassName="active">Busket</NavLink>
+          <NavLink to='/admin-panel' className="navbar-link ms-4" activeClassName="active">Admin Panel</NavLink>
+        </div>
+      </div>
+    </div>
+  </nav>
 );
 
 Header.propTypes = {};
