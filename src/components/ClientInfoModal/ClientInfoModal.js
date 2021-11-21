21013caea14b19702cb625a33fca339166434f10
mrzhenshen <mrzhenshen@gmail.com>
2021-11-21 21:08:00 +0200

    setup modal for client detail

--- src/components/ClientInfoModal/ClientInfoModal.js
+++ src/components/ClientInfoModal/ClientInfoModal.js
@@ -2,11 +2,50 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import './ClientInfoModal.scss';
 
-const ClientInfoModal = () => (
-  <div className="ClientInfoModal">
-    ClientInfoModal Component
-  </div>
-);
+import { Button, Modal } from 'react-bootstrap';
+
+class ClientInfoModal extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.handleClose = this.handleClose.bind(this)
+  }
+
+  handleClose() {
+    this.props.onClickClose()
+  }
+
+  render() {
+    return (
+      <Modal show={this.props.show} onHide={this.handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>
+            {this.props.client.first_name} {this.props.client.last_name}
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>{this.props.client.id}</p>
+          <p>{this.props.client.birth_date}</p>
+          <p>{this.props.client.telephone_number}</p>
+          <p>{this.props.client.email}</p>
+          <p>{this.props.client.is_staff}</p>
+          <p>{this.props.client.company_name}</p>
+          <p>{this.props.client.company_link}</p>
+          <p>{this.props.client.industry}</p>
+          <p>{this.props.client.role}</p>
+          <p>{this.props.client.country}</p>
+          <p>{this.props.client.city}</p>
+          
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary" onClick={this.handleClose}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    )
+  }
+}
 
 ClientInfoModal.propTypes = {};
 
