96969a9276b48b6401f5145a2c43212ac1856432
mrzhenshen <mrzhenshen@gmail.com>
2021-12-02 10:00:38 +0200

    convert variable component to class

--- src/pages/Landing/Landing.js
+++ src/pages/Landing/Landing.js
@@ -1,53 +1,56 @@
 import React, { useEffect, useState } from 'react';
-import PropTypes from 'prop-types';
 import './Landing.scss';
 
-const Landing = () => {
+class Landing extends React.Component {
 
-  const [goods, setGoods] = useState([])
+  constructor(props) {
+    super(props);
 
-  useEffect(() => {
+    this.state = {
+      goods: []
+    }
+  }
+
+  componentDidMount() {
     fetch('http://localhost:8000/api/goods/')
       .then((response) => {
         return response.json()
       })
       .then((data) => {
-        setGoods(data)
+        console.log(data)
+        this.setState({ goods: data })
       })
-  }, [goods])
+  }
 
-  return (
+  render() {
+    return (
+      <div className="Landing" >
+        <center><h2>Landing Component</h2></center>
+        <div className='container'>
+          <div className="row">
+            <div className="col-12">
+              <h3>Goods</h3>
+            </div>
 
-    <div className="Landing">
-      <center><h2>Landing Component</h2></center>
-      <div className='container'>
-        <div className="row">
-          <div className="col-12">
-            <h3>Goods</h3>
-          </div>
-
-          {goods.map(el =>
-            <div className="col-4" key={el.id}>
-              <div className="card w-18">
-                <img src={el.example_image} alt="Example image" />
-                <div className="card-body">
-                  <h5 className="card-title">{el.name}</h5>
-                  <p className="card-text">{el.description}</p>
-                  <h6>{el.pricing}</h6>
-                  <button className="btn btn-primary">Order</button>
+            {this.state.goods.map(el =>
+              <div className="col-4" key={el.id}>
+                <div className="card w-18">
+                  <img src={el.example_image} alt="Example image" />
+                  <div className="card-body">
+                    <h5 className="card-title">{el.name}</h5>
+                    <p className="card-text">{el.description}</p>
+                    <h6>{el.pricing}</h6>
+                    <button className="btn btn-primary">Order</button>
+                  </div>
                 </div>
               </div>
-            </div>
-          )}
-          
+            )}
+
+          </div>
         </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
-
-Landing.propTypes = {};
-
-Landing.defaultProps = {};
 
 export default Landing;
