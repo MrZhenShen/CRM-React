3ebf6c605bfd855f8fe318a2c046e44472dee031
maxloskut <lem123squizy@gmail.com>
2021-12-03 15:59:40 +0200

    create register page

---
+++ src/pages/RegisterPage/RegisterPage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import './RegisterPage.scss';
+
+const RegisterPage = () => (
+  <div className="RegisterPage">
+    RegisterPage Component
+  </div>
+);
+
+RegisterPage.propTypes = {};
+
+RegisterPage.defaultProps = {};
+
+export default RegisterPage;
