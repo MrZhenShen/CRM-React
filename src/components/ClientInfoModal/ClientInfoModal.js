9a7d77bcaa682ae9b376b9e06aae2c4197d288ce
mrzhenshen <mrzhenshen@gmail.com>
2021-12-22 11:28:34 +0200

    fix modal abotu client

--- src/components/ClientInfoModal/ClientInfoModal.js
+++ src/components/ClientInfoModal/ClientInfoModal.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import './ClientInfoModal.scss';
 
 import { Button, Modal } from 'react-bootstrap';
@@ -8,23 +7,35 @@ class ClientInfoModal extends React.Component {
 
   constructor(props) {
     super(props)
-    this.handleClose = this.handleClose.bind(this)
+
   }
 
-  handleClose() {
-    this.props.onClickClose()
+  handleHide() {
+    this.props.onHide()
   }
 
   render() {
     return (
-      <Modal show={this.props.show} onHide={this.handleClose}>
+      <Modal
+        show={this.props.show}
+        onHide={this.handleClose}
+        {...this.props}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered>
         <Modal.Header closeButton>
           <Modal.Title>
-            {this.props.client.first_name} {this.props.client.last_name}
+            Anton Antonov
+            {/* {this.props.client.first_name} {this.props.client.last_name} */}
           </Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          <p>{this.props.client.id}</p>
+          <h2>Anton Antonov</h2>
+          <h6>id: 61616272</h6>
+          <p>Company: SoftServe</p>
+          <p>anton@mail.com</p>
+          <p>+38000999111</p>
+          {/* <p>{this.props.client.id}</p>
           <p>{this.props.client.birth_date}</p>
           <p>{this.props.client.telephone_number}</p>
           <p>{this.props.client.email}</p>
@@ -34,11 +45,11 @@ class ClientInfoModal extends React.Component {
           <p>{this.props.client.industry}</p>
           <p>{this.props.client.role}</p>
           <p>{this.props.client.country}</p>
-          <p>{this.props.client.city}</p>
-          
+          <p>{this.props.client.city}</p> */}
+
         </Modal.Body>
         <Modal.Footer>
-          <Button variant="primary" onClick={this.handleClose}>
+          <Button variant="primary" onClick={this.handleHide.bind(this)}>
             Close
           </Button>
         </Modal.Footer>
@@ -47,8 +58,5 @@ class ClientInfoModal extends React.Component {
   }
 }
 
-ClientInfoModal.propTypes = {};
-
-ClientInfoModal.defaultProps = {};
 
 export default ClientInfoModal;
