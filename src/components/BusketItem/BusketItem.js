c3cffc3e57d7eda0bddef27636735d85f1f2fcd0
mrzhenshen <mrzhenshen@gmail.com>
2021-12-26 15:08:40 +0200

    create Busket list staff

---
+++ src/components/BusketItem/BusketItem.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './BusketItem.scss';
+
+const BusketItem = () => (
+  <div className="BusketItem">
+    BusketItem Component
+  </div>
+);
+
+export default BusketItem;
---
+++ src/components/BusketList/BusketList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './BusketList.scss';
+
+const BusketList = () => (
+  <div className="BusketList">
+    BusketList Component
+  </div>
+);
+
+export default BusketList;
