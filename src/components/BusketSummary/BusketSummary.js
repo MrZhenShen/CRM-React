ca60c1a809cddaa71c76df07902ccec09890de32
mrzhenshen <mrzhenshen@gmail.com>
2021-12-26 19:54:24 +0200

    create BusketSummary

---
+++ src/components/BusketSummary/BusketSummary.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './BusketSummary.scss';
+
+const BusketSummary = () => (
+  <div className="BusketSummary">
+    BusketSummary Component
+  </div>
+);
+
+export default BusketSummary;
