efad3869582e84c64d5c4127c07d6af2a638a310
mrzhenshen <mrzhenshen@gmail.com>
2021-12-28 19:12:48 +0200

    init footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import './Footer.scss';
 
+import { Navbar, Container } from 'react-bootstrap'
+
 const Footer = () => (
-  <div className="Footer"></div>
+  <Navbar fixed="bottom" expand="lg" bg="dark" variant="dark">
+    <Container fluid>
+      <Navbar.Text>
+        Copyright - Yevhen Rozhylo (3CS-21) - 2021
+      </Navbar.Text>
+    </Container>
+  </Navbar>
 );
 
 export default Footer;
