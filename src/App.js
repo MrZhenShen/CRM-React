2d0f0376d6a9a04ae3c70ff39f1877c28707261a
mrzhenshen <mrzhenshen@gmail.com>
2021-12-28 12:57:35 +0200

    remove auth page from router

--- src/App.js
+++ src/App.js
@@ -7,9 +7,7 @@ import Footer from './components/Footer/Footer'
 import LandingPage from './pages/LandingPage/LandingPage'
 import AdminPanel from './pages/AdminPanel/AdminPanel'
 import BusketPage from './pages/BusketPage/BusketPage'
-import LoginPage from './pages/LoginPage/LoginPage'
 import ClientWorkspacePage from './pages/ClientWorkspacePage/ClientWorkspacePage'
-import RegisterPage from './pages/RegisterPage/RegisterPage'
 
 function App() {
   return (
@@ -22,9 +20,7 @@ function App() {
             <Route path="/landing" element={<LandingPage />} />
             <Route path="/busket" element={<BusketPage />} />
             <Route path="/admin-panel" element={<AdminPanel />} />
-            <Route path="/login" element={<LoginPage />} />
             <Route path="/client-workspace" element={<ClientWorkspacePage />} />
-            <Route path="/register" element={<RegisterPage />} />
           </Routes>
         </div>
         <Footer />
